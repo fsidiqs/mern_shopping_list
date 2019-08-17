dfba1491d99541855c85d41e47b75e162e6d9031
fajar <fsidiqs@hotmail.co.id>
2019-08-17 07:24:34 +0700

    added Authentication and access control

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,7 +6,12 @@ import {getItems, deleteItem} from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component{
-
+    
+    static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+    }
     componentDidMount() {
         this.props.getItems();
     }
@@ -25,7 +30,16 @@ class ShoppingList extends Component{
                     {items.map(({_id, name}) => (
                             <CSSTransition timeout={500} key={_id}  classNames="fade" >
                                 <ListGroupItem >
-                                <Button className="remove-btn" color="danger" size="sm" onClick={()=>(this.onDeleteClick(_id))}>&times;</Button>
+                                    {this.props.isAuthenticated ? 
+                                    <Button 
+                                        className="remove-btn" 
+                                        color="danger" 
+                                        size="sm" 
+                                        onClick={()=>(this.onDeleteClick(_id))}
+                                    >
+                                    &times;
+                                    </Button> : null
+                                    }   
                                 {name}
                                 </ListGroupItem>
                             </CSSTransition>
@@ -37,13 +51,10 @@ class ShoppingList extends Component{
     }
 }
 
-ShoppingList.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}
 
 const mapStatetoProps = (state) => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStatetoProps, {getItems, deleteItem})(ShoppingList);
